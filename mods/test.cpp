76c3a4beb73b2a340df258e3c3c92625a99a432a
h1fired <misha01213@gmail.com>
2026-03-12 17:09:13 +0200

    Added serial

---
+++ mods/test.cpp
@@ -0,0 +1,17 @@
+#include "serial.h"
+#include <iostream>
+
+
+int main() {
+    Serial serial("/dev/ttyS0", 115200);
+
+    if (!serial.open()) {
+        std::cerr << "Failed to open port\n";
+        return 1;
+    }
+
+    while (true) {
+        std::string line = serial.readLine();
+        std::cout << "Got: " << line << "\n";
+    }
+}
