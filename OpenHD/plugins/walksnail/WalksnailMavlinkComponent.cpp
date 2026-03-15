b87cf0c71b1c30ddf76dbc4ee8d4b239f3868918
h1fired <misha01213@gmail.com>
2026-03-15 16:42:53 +0200

    Added test prints

--- OpenHD/plugins/walksnail/WalksnailMavlinkComponent.cpp
+++ OpenHD/plugins/walksnail/WalksnailMavlinkComponent.cpp
@@ -1,5 +1,5 @@
 #include "WalksnailMavlinkComponent.h"
-
+#include <iostream>
 
 WalksnailMavlinkComponent::WalksnailMavlinkComponent(
     uint8_t sys_id
@@ -10,12 +10,14 @@ WalksnailMavlinkComponent::WalksnailMavlinkComponent(
 std::vector<MavlinkMessage> WalksnailMavlinkComponent::process_mavlink_messages(
     std::vector<MavlinkMessage> messages)
 {
+    std::cout << "WSMC process" << std::endl;
     std::vector<MavlinkMessage> ret;
     return ret;
 }
 
 std::vector<MavlinkMessage> WalksnailMavlinkComponent::generate_mavlink_messages()
 {
+    std::cout << "WSMC generate" << std::endl;
     std::vector<MavlinkMessage> ret;
 
     // optional heartbeat
