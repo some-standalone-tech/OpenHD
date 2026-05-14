8eb873a10fb23dfcffc73f8ac8b4257c5cf9c404
h1fired <misha01213@gmail.com>
2026-05-14 12:14:35 +0300

    Changed serial config

--- OpenHD/plugins/walksnail/serial/serial.cpp
+++ OpenHD/plugins/walksnail/serial/serial.cpp
@@ -5,7 +5,6 @@
 #include <stdexcept>
 #include <sys/ioctl.h>
 
-
 Serial::Serial(const std::string& port, int baudrate)
     : m_port(port), m_baud(baudrate), m_fd(-1) {}
 
@@ -27,14 +26,24 @@ bool Serial::open()
     cfsetispeed(&tty, spd);
     cfsetospeed(&tty, spd);
 
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
-    tty.c_cflag &= ~(PARENB | CSTOPB | CRTSCTS);
-    tty.c_cflag |=  (CREAD | CLOCAL);
-    tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY | ICRNL);
-    tty.c_oflag &= ~OPOST;
-    tty.c_cc[VMIN]  = 1;
-    tty.c_cc[VTIME] = 0;
+    tty.c_cflag
+        = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit characters
+    tty.c_iflag &= ~IGNBRK; // disable break processing
+    tty.c_lflag = 0; // no signaling chars, no echo, no
+                     // canonical processing
+    tty.c_oflag = 0; // no remapping, no delays
+    tty.c_cc[VMIN] = 0; // read doesn't block
+    tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF
+                     | IXANY); // shut off xon/xoff ctrl
+
+    tty.c_cflag
+        |= (CLOCAL | CREAD); // ignore modem controls,
+                             // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD); // shut off parity
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
 
     if (tcsetattr(m_fd, TCSANOW, &tty) != 0) {
         ::close(m_fd);
